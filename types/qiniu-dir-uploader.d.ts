67bdb551d89d9453aa5bc1a77c851cb469ae867a
William <dwy666@hotmail.com>
2018-02-07 10:47:05 +0800

    initial commit

---
+++ types/qiniu-dir-uploader.d.ts
@@ -0,0 +1,55 @@
+import qiu from 'qiniu'
+import log4js from 'log4js'
+
+interface UploadOption {
+  /**
+   * 空间名
+   */
+  bucket: string;
+
+  /**
+   * accessKey
+   */
+  accessKey: string;
+  /**
+   * secretKey
+   */
+  secretKey: string;
+  /**
+   * 要排除的文件后缀名
+   */
+  excludes: string[];
+  /**
+   * 打包资源根目录
+   */
+  buildAssetsRoot: string;
+  /**
+   * 发布资源根目录，（即七牛上的目录）
+   */
+  publishAssetsRoot: string;
+
+  /**
+   * log4js category
+   */
+  loggerCategory: string;
+
+  /**
+   * 机房位置
+   */
+  zone:string;
+}
+
+interface ReaddirCallback{
+  (filePath:string);
+}
+
+export class QiniuDirUploader{
+  config:UploadOption;
+  uploadToken:string;
+  formUploader:qiu.form_up.FormUploader;
+  logger:log4js;
+  constructor(option:UploadOption);
+  uploadFile(key:string,localFile:string):void;
+  readDir(callback:ReaddirCallback):void;
+  upload():void;
+}
